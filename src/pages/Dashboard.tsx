d55a61cd3be8f02722cf4d50ac46d5cc79a7689e
Bruno Porto <bruno.porto@mettabr.local>
2025-07-22 15:34:27 -0300

    tirei o titulo teste

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -75,7 +75,7 @@ export default function Dashboard() {
     <Layout currentPage="dashboard">
       <div className="space-y-6">
         <div>
-          <h1 className="text-2xl font-bold text-foreground">Dashboard TESTE</h1>
+          <h1 className="text-2xl font-bold text-foreground">Dashboard</h1>
           <p className="text-muted-foreground">Controle de ponto e projetos</p>
         </div>
 
